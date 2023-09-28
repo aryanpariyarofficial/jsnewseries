3976b9f01fa8f1c6e54f02c8c81d92f0fb4aa955
Aryan Pariyar <78752170+aryanpariyarofficial@users.noreply.github.com>
2023-09-28 16:34:02 +0000

    started datatypes

---
+++ basic_01/datatypes.js
@@ -0,0 +1,9 @@
+"use strict" //treat all JS code as newer version
+ 
+// alert(3+5) // we are using nodejs not browser.
+
+
+// There a several datatypes in JS.
+ let name = "Aryan";
+ let a = 4;
+console.log(name +" is " +a+" Years old.");
